2b7a9335c621795518d7214a69b4c8efb480ccf9
cond3nz <67028832+cond3nz@users.noreply.github.com>
2023-10-18 01:21:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo pacman -S dunst fish helix kitty xorg-xwayland qutebrowser ranger kvantum rofi pamixer terminus-font w3m
+sudo pacman -S dunst fish helix kitty xorg-xwayland qutebrowser ranger rofi pamixer terminus-font w3m
 cp -rf .config/* ~/.config/
 sudo chsh $USER -s /bin/fish
