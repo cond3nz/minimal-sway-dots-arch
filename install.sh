2dcd68b22a0600033f25ae8953075343ad457e46
cond3nz <67028832+cond3nz@users.noreply.github.com>
2023-10-18 02:30:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-yay -S dunst fish helix kitty xorg-xwayland qutebrowser ranger rofi pamixer terminus-font w3m gtk2 qt5-styleplugins qt6gtk2 
+yay -S dunst fish helix kitty xorg-xwayland qutebrowser ranger rofi pamixer terminus-font w3m gtk2 gtk3 qt5gtk2 qt6gtk2 
 cp -rf .config/* ~/.config/
 sudo chsh $USER -s /bin/fish
