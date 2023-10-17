b55faa5060ab832d1db569458baef228bccd6781
cond3nz <67028832+cond3nz@users.noreply.github.com>
2023-10-18 01:28:13 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     export XDG_SESSION_TYPE=wayland
     export GDK_BACKEND=wayland
+    export QT_QPA_PLATFORMTHEME=gtk2
 end
