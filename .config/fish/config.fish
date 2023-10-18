a95267245beba5566831f898e7e5e1f64e009b5e
cond3nz <tay93376@yandex.ru>
2023-10-18 03:29:41 +0300

    add themes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    export XDG_SESSION_TYPE=wayland
-    export GDK_BACKEND=wayland
-    export QT_QPA_PLATFORMTHEME=gtk2
 end
