fd269e3f3b8efd795d1b6f06e72653061a3ad71e
cond3nz <tay93376@yandex.ru>
2023-10-18 03:48:08 +0300

    add themes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-yay -S dunst fish helix kitty xorg-xwayland qutebrowser ranger rofi pamixer terminus-font w3m gtk2 gtk3 qt5-styleplugins qt6gtk2 
+yay -S dunst fish helix kitty xorg-xwayland qutebrowser ranger rofi pamixer terminus-font w3m gtk2 gtk3 qt5-styleplugins qt6gtk2 gtk-engine-murrine
 cp -rf .config/* ~/.config/
 mkdir -p ~/.icons && cp -rf .icons/* ~/.icons/
 mkdir -p ~/.themes && cp -rf .themes/* ~/.themes/
